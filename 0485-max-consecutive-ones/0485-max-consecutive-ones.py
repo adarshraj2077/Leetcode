b490687bb6ac899eeb99e2cd71ca0b24e4cbf645
Adarsh Raj <m10raj10@gmail.com>
2026-08-31 23:51:42 +0530

    Time: 10 ms (88.09%), Space: 22 MB (5.05%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -3,8 +3,8 @@ class Solution:
         count = 0
         max_count = 0
 
-        for i in range(len(nums)):
-            if nums[i] == 1:
+        for num in nums:
+            if num == 1:
                 count += 1
             else:
                 count = 0
