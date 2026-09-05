a0f511cc83138c36195d9b75109e2ab2db303f81
Adarsh Raj <m10raj10@gmail.com>
2026-09-05 15:27:38 +0530

    Time: 3 ms (18.43%), Space: 19.3 MB (80.99%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -2,14 +2,14 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
 
-        for op in operations:
-            if op == "C":
+        for operation in operations:
+            if operation == "+":
+                stack.append(stack[-1]+stack[-2])
+            elif operation == "D":
+                stack.append(2*stack[-1])
+            elif operation == "C":
                 stack.pop()
-            elif op == "D":
-                stack.append(stack[-1]*2)
-            elif op == "+":
-                stack.append(stack[-1] + stack[-2])
             else:
-                stack.append(int(op))
-
+                stack.append(int(operation))
+        
         return sum(stack)
