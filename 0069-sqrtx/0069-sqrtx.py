c507ff71e325b84ae26d045416f2b1553a9f0bf9
Adarsh Raj <m10raj10@gmail.com>
2026-09-13 15:51:06 +0530

    Time: 0 ms (100%), Space: 19.4 MB (21.16%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low = 0
+        high = x
+        ans = 0
+
+        while low <= high:
+            mid = (low+high) // 2
+
+            if mid*mid == x:
+                return mid
+            
+            if mid*mid > x:
+                high = mid - 1
+            else:
+                ans = mid
+                low = mid + 1
+
+        return ans
